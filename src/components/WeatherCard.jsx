79eab9045001421eefa8f2d397b38837de74020d
unknown <you@example.com>
2024-06-06 17:01:56 -0400

    actulizacion de la temperatura

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,57 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ButtonLocation } from './ButtonLocation'
 
 export const WeatherCard = ({data, toggleModal}) => {
-
 
   const [currentTemperature, setCurrentTemperature] = useState('');
   const [currentWeatherDescription, setCurrentWeatherDescription] = useState('');
   const [currentDate, setCurrentDate] = useState('');
 
-  useEffect(() => {
-      const updateWeather = () => {
-          if (!data || !data.list || data.list.length === 0) {
-              return;
-          }
+  useEffect(()=>{
+    const updateWeather = () =>{
+      if (!data || !data.list || data.list.length === 0) {
+        return
+      }
+      const currentWeather = data.list[0];
+      if (!currentWeather.main || !currentWeather.weather || currentWeather.weather.length === 0) {
+        return;
+    }
+    const temperature = currentWeather.main.temp;
+    const weatherDescription = currentWeather.weather[0].description;
+    const temperatureInCelsius = kelvinToCelsius(temperature);
+    setCurrentTemperature(temperatureInCelsius.toFixed(2));
+    setCurrentWeatherDescription(weatherDescription);
 
-          const currentWeather = data.list[0];
+    setCurrentDate(getCurrentDate());
+    }
+    const kelvinToCelsius = (temp) => {
+      return temp - 273.15;
+  }
 
-          if (!currentWeather.main || !currentWeather.weather || currentWeather.weather.length === 0) {
-              return;
-          }
+  const getCurrentDate = () => {
+      const date = new Date();
+      const options = { weekday: 'long', month: 'short', day: 'numeric' };
+      return date.toLocaleDateString('en-US', options);
+  }
 
-          const temperature = currentWeather.main.temp;
-          const weatherDescription = currentWeather.weather[0].description;
-          const temperatureInCelsius = kelvinToCelsius(temperature);
-          setCurrentTemperature(temperatureInCelsius.toFixed(2));
-          setCurrentWeatherDescription(weatherDescription);
-
-          // Actualizar la fecha actual
-          setCurrentDate(getCurrentDate());
-      };
-
-      const kelvinToCelsius = (temp) => {
-          return temp - 273.15;
-      };
-
-      const getCurrentDate = () => {
-          const date = new Date();
-          const options = { weekday: 'long', month: 'short', day: 'numeric' };
-          return date.toLocaleDateString('en-US', options);
-      };
-
-      // Llamar a la función de actualización del clima cuando se monte el componente
-      updateWeather();
-
-      // Configurar un temporizador para que se actualice cada 3 horas
-      const interval = setInterval(() => {
           updateWeather();
-      }, 3 * 60 * 60 * 1000); // 3 horas en milisegundos
 
-      // Limpiar el temporizador cuando el componente se desmonte para evitar fugas de memoria
-      return () => clearInterval(interval);
-
-  }, [data]);
+          const interval = setInterval(() => {
+              updateWeather();
+          }, 3 * 60 * 60 * 1000); 
+  
+          return () => clearInterval(interval);
+  },[data])
+      
 
   return (
     <div className="relative h-screen w-full md:max-w-[390px] mx-auto bg-[#1E213A] text-white ">
@@ -68,28 +59,30 @@ export const WeatherCard = ({data, toggleModal}) => {
         </figure>
 
         <figure className='w-48 absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 z-50'>
-          <img src="" alt="" />
+          <img src={`/public/${data && data.list && data.list.length > 0 && data.list[0].weather[0].icon}.png`} alt="" />
         </figure>
       </div>
 
       <div className='mb-14 text-center'>
         <div className=" text-[144px]  leading-none text-center inline-block">
-          <span className="text-[#e7e7eb] text-9xl font-serif mt-6">15</span>
+          <span className="text-[#e7e7eb] text-9xl font-Raleway mt-6">{currentTemperature}</span>
           <span className="text-5xl font-normal text-[#A09FB1] ">°C</span>
         </div>
       </div>
 
       <div className="text-[#A09FB1] text-center text-4xl mt-6 mb-12">
-        <span className=''>Shower</span>
+        <span >{currentWeatherDescription}</span>
       </div>
 
       <div className='text-[#88869D] text-center mb-4'>
-        <p className=''>Today  •  Wed, 5 Jun</p>
+        <p>{currentDate}</p>
         <div className='flex justify-center mt-10'>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-            <path fillRule="evenodd" d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" clipRule="evenodd" />
+            <path fillRule="evenodd"
+             d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+             clipRule="evenodd" />
           </svg>
-          <span className='ml-2'>Location</span>
+          <span className='ml-2'>{data && data.city && data.city.name || 'Select a location'}</span>
         </div>
       </div>
     </div>

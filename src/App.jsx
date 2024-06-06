63b2e68540d56886ccd4ac8b466bb8c6c960d09d
unknown <you@example.com>
2024-06-05 22:51:32 -0400

    finalizado el Custom Hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { DataProvide } from './hooks/DataProvide'
 import { WeatherCard } from './components/WeatherCard'
+import { ClimateTime } from './components/ClimateTime'
 
 function App() {
 
@@ -9,8 +10,8 @@ function App() {
     <DataProvide>
       <div className="min-h-screen flex flex-col md:flex-row bg-[#100E1D] text-white">
         <WeatherCard/>
+        <ClimateTime/>
       </div>
-
     </DataProvide>
 
 
--- src/hooks/DataProvide.jsx
+++ src/hooks/DataProvide.jsx
@@ -2,28 +2,78 @@ import { createContext, useEffect, useState } from "react";
 
 export const dataContext = createContext()
 
-export const DataProvide = ({children}) =>{
-    const[data, setData] = useState([])
-    const[city, setCity] = useState('')
+export const DataProvide = ({ children }) => {
+    const [data, setData] = useState([])
+    const [city, setCity] = useState('')
+    const [lotation, setLocation] = useState([])
 
-   /*  const getData = async (lat, lon) =>{
-        const rs = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=c739b37edac93e5b65459fa362b12a26`)
+    //Se obtine los datos del clima por longitud y latitud 
+    const getData = async (lat, lon) => {
+        try {
+            const rs = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=9174ddaf8fa6b3aa86b127d7c27fd64d`)
+            const rsJson = await rs.json()
+            setData(rsJson)
+        } catch (error) {
+            console.error('Error fetching weather data:', error)
+        }
+    }
+    
+    //Se obtiene los datos por el nombre de la ciudad
+    const getDataByCty = async (cityName) => {
+        try {
+        const rs = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=9174ddaf8fa6b3aa86b127d7c27fd64d`)
         const rsJson = await rs.json()
-        setData(rsJson)
-    } */
- /*    const cityName = async (lat, lon) =>{
-        const rs = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=57cc648f410e0c7a6314ac6aaa175edf`)
-    } */
+        setData(rsJson)            
+        } catch (error) {
+            console.error('Error fetching city name weather:', error)
+        }
 
+    }
 
-/*     useEffect(() =>{
-        getData()
-    },[])   */
-    return(
+    //Se obtiene los datos por el nombre de la ciudad 
+    const getCityName = async (lat, lon) => {
+        try {
+        const rs = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9174ddaf8fa6b3aa86b127d7c27fd64d`)
+        const rsJson = await rs.json()
+        setCity(rsJson.name)            
+        } catch (error) {
+            console.error('Error fetching city name:', error)
+        }
+
+    }
+
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setLocation({
+                        lat: position.coords.latitude,
+                        lon: position.coords.longitude,
+                    })
+                }
+            )
+
+        }
+    }
+
+    useEffect(() => {
+        getLocation()
+    }, [])
+
+    useEffect(() => {
+        if (lotation) {
+            getData(location.lat, location.lon);
+            getCityName(location.lat, location.lon);
+        }
+    }, [])
+    return (
         <dataContext.Provider value={{
-            data
+            data,
+            city,
+            getLocation,
+            setLocation,
+            getDataByCty
         }}>
-            
             {children}
         </dataContext.Provider>
     )
